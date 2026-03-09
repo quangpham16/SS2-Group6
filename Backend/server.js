44f601094123b34aff4abb94651843eab2579264
quangpham16 <quangpham16082005@gmail.com>
2026-03-09 16:52:41 +0700

    First Commit

---
+++ Backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.get('api/status', (req,res) => { ... })
