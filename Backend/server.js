87588b08245661589111a32b98e13a3468c45a6f
quangpham16 <quangpham16082005@gmail.com>
2026-03-13 21:03:05 +0700

    done

--- Backend/server.js
+++ Backend/server.js
@@ -5,12 +5,13 @@ require('dotenv').config()
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.get('api/status', (req,res) =>{res.json({message: "Hehehehe"});
-});
+app.get('/api/status', (req,res) =>{res.json({message: "Hehehehe"});
+})
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
 console.log ("Thank you")
 
 } )
+
 
